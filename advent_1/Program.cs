8556b0175d8bb8082bf9374693eafa436420987c
hummusbird <birb@birb.cc>
2023-11-29 12:27:50 +0000

    advent 1

---
+++ advent_1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace advent_1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> depths = File.ReadAllText("input.txt").Split("\n").Select(int.Parse).ToList();
+
+        int increasedCount = 0;
+
+        for (int i = 0; i < depths.Count; i++)
+        {
+            Console.Write(depths[i]);
+            if (i == 0)
+            {
+                Console.WriteLine(" - no previous measurement");
+            }
+            else if (depths[i] > depths[i - 1])
+            {
+                increasedCount++;
+                Console.WriteLine(" - increased");
+            }
+            else
+            {
+                Console.WriteLine(" - decreased");
+            }
+        }
+
+        Console.WriteLine("amount of increases: " + increasedCount);
+    }
+}
---
+++ advent_2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace advent_2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> depths = File.ReadAllText("input.txt").Split("\n").Select(int.Parse).ToList();
+
+        int increasedCount = 0;
+
+        for (int i = 0; i < depths.Count - 3; i++)
+        {
+            if (addthree(i, depths) < addthree(i + 1, depths))
+            {
+                increasedCount++;
+            }
+        }
+        Console.WriteLine("number of increases: " + increasedCount);
+    }
+
+    static int addthree(int index, List<int> depths)
+    {
+        int value = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            value += depths[index + i];
+        }
+        return value;
+    }
+}
